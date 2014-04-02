fe482c85b071e919cfcbe6b0ff2e2f6cd323c0bf
Marcus <marcusronnang@hotmail.com>
2014-04-02 19:26:39 +0200

    Working on top menu hover

--- app/code/local/MarcusRonnang/Layout/Block/Topmenu.php
+++ app/code/local/MarcusRonnang/Layout/Block/Topmenu.php
@@ -64,11 +64,11 @@ class MarcusRonnang_Layout_Block_Topmenu extends Mage_Page_Block_Html_Topmenu
 			$html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
 			if(counter==1){
 				$html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . ' class="first" ><span>'
-                . $this->escapeHtml($child->getName()) . '</span><span class="down_triangle"></span></a>';	
+                . $this->escapeHtml($child->getName()) . '</span></a><span class="down_triangle"></span>';	
 			}
 			else{
 				$html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . '><span>'
-                . $this->escapeHtml($child->getName()) . '</span><span class="down_triangle"></span></a>';	
+                . $this->escapeHtml($child->getName()) . '</span></a><span class="down_triangle"></span>';	
 			}
             
             
