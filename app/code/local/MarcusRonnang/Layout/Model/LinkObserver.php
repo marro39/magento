21c382284c4776dadc88f7f003c5ed3218cf64a7
Marcus <marcusronnang@hotmail.com>
2014-05-04 13:06:00 +0200

    Created observerclass in marcusronnang_layout module, linkobserver.php

--- app/code/local/MarcusRonnang/Layout/Model/LinkObserver.php
+++ app/code/local/MarcusRonnang/Layout/Model/LinkObserver.php
@@ -2,32 +2,32 @@
 class MarcusRonnang_Layout_Model_LinkObserver extends Varien_Event_Observer{
 		
 	public function addToTopmenu(Varien_Event_Observer $observer)
-{
-    $menu = $observer->getMenu();
-    $tree = $menu->getTree();
-    $node = new Varien_Data_Tree_Node(array(
-            'name'   => 'Categories',
-            'id'     => 'categories',
-            'url'    => Mage::getUrl(), // point somewhere
-    ), 'id', $tree, $menu);
-    $menu->addChild($node);
-    // Children menu items
-    $collection = Mage::getResourceModel('catalog/category_collection')
-            ->setStore(Mage::app()->getStore())
-            ->addIsActiveFilter()
-            ->addNameToResult()
-            ->setPageSize(3);
-    foreach ($collection as $category) {
-        $tree = $node->getTree();
-        $data = array(
-            'name'   => $category->getName(),
-            'id'     => 'category-node-'.$category->getId(),
-            'url'    => $category->getUrl(),
-        );
-        $subNode = new Varien_Data_Tree_Node($data, 'id', $tree, $node);
-        $node->addChild($subNode);
-    }
-}
+	{
+	    $menu = $observer->getMenu();
+	    $tree = $menu->getTree();
+	    $node = new Varien_Data_Tree_Node(array(
+	            'name'   => 'Categories',
+	            'id'     => 'categories',
+	            'url'    => Mage::getUrl(), // point somewhere
+	    ), 'id', $tree, $menu);
+	    $menu->addChild($node);
+	    // Children menu items
+	    $collection = Mage::getResourceModel('catalog/category_collection')
+	            ->setStore(Mage::app()->getStore())
+	            ->addIsActiveFilter()
+	            ->addNameToResult()
+	            ->setPageSize(3);
+	    foreach ($collection as $category) {
+	        $tree = $node->getTree();
+	        $data = array(
+	            'name'   => $category->getName(),
+	            'id'     => 'category-node-'.$category->getId(),
+	            'url'    => $category->getUrl(),
+	        );
+	        $subNode = new Varien_Data_Tree_Node($data, 'id', $tree, $node);
+	        $node->addChild($subNode);
+	    }
+	}
 		
 }
 ?>
