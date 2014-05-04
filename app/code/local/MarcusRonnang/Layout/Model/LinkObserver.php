59e088fee405176b84da553cadde11070bc774c9
Marcus <marcusronnang@hotmail.com>
2014-05-04 13:11:43 +0200

    Created observerclass in marcusronnang_layout module, linkobserver.php

--- app/code/local/MarcusRonnang/Layout/Model/LinkObserver.php
+++ app/code/local/MarcusRonnang/Layout/Model/LinkObserver.php
@@ -1,6 +1,6 @@
 <?php
 class MarcusRonnang_Layout_Model_LinkObserver{
-		
+	/*	
 	public function addToTopmenu(Varien_Event_Observer $observer)
 	{
 	    $menu = $observer->getMenu();
@@ -28,6 +28,24 @@ class MarcusRonnang_Layout_Model_LinkObserver{
 	        $node->addChild($subNode);
 	    }
 	}
+	*/
+	public function addItemsToTopmenuItems($observer) {
+        $menu = $observer->getMenu();
+        $tree = $menu->getTree();
+        $action = Mage::app()->getFrontController()->getAction()->getFullActionName();
+
+        $nodeId = 'some-node-id';
+        $data = array(
+            'name' => Mage::helper('[module]')->__('Title goes here'),
+            'id' => $nodeId,
+            'url' => Mage::getUrl('module/controller/action'),
+            'is_active' => ($action == 'module_controller_action')
+        );
+        $node = new Varien_Data_Tree_Node($data, 'id', $tree, $menu);
+        $menu->addChild($node);
+        return $this;
+    }
+	
 		
 }
 ?>
