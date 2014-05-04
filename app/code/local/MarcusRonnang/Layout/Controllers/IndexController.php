76c6a32bfd1ae31681778621b126faa114fae9bf
Marcus <marcusronnang@hotmail.com>
2014-05-04 17:39:53 +0200

    Created controllerclass for laptops in meny, IndexController.php

--- app/code/local/MarcusRonnang/Layout/Controllers/IndexController.php
+++ app/code/local/MarcusRonnang/Layout/Controllers/IndexController.php
@@ -2,7 +2,8 @@
     class MarcusRonnang_Layout_IndexController extends Mage_Core_Controller_Front_Action{
     	
     	public function indexAction(){
-    		var_dump(__METHOD__);
+    		$this->loadLayout();
+    		$this->renderLayout();
     	} 
     }
 ?>
