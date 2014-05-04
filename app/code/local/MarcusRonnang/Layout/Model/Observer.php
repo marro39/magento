48b8d700d9f6c959a61220d805c8b2260a295c33
Marcus <marcusronnang@hotmail.com>
2014-05-04 18:53:27 +0200

    Created controllerclass for laptops in meny, IndexController.php

--- app/code/local/MarcusRonnang/Layout/Model/Observer.php
+++ app/code/local/MarcusRonnang/Layout/Model/Observer.php
@@ -11,7 +11,7 @@ class MarcusRonnang_Layout_Model_Observer extends Varien_Event_Observer{
 	    $node = new Varien_Data_Tree_Node(array(
 	            'name'   => 'Laptops',
 	            'id'     => 'laptops',
-	            'url'    => Mage::getUrl() . 'marcusronnang_layout/index/index', // point somewhere
+	            'url'    => Mage::getUrl() . 'marcusronnang_layout/index/index/', // point somewhere
 	            /*'url'    => Mage::getUrl(), // point somewhere*/
 	    ), 'id', $tree, $menu);	    
 	    $menu->addChild($node);
