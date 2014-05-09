d55ab4c3e51bcbff1601c54cb87ad646d2d3a2e2
Marcus <marcusronnang@hotmail.com>
2014-05-09 23:12:40 +0200

    working with indexController.php, loading category page

--- app/code/local/MarcusRonnang/App/controllers/IndexController.php
+++ app/code/local/MarcusRonnang/App/controllers/IndexController.php
@@ -10,17 +10,20 @@
 			$this->loadLayout();
 			$this->renderLayout();
 			*/
-			$this->loadLayout();			
+			$this->loadLayout('category.products');
+			$this->renderLayout();			
 			
+			
+			/*
 			$this->getLayout()->createBlock('root')->setTemplate('page/3columns.phtml');			
 			//$this->getLayout()->getBlock('content')->append($rootBlock);
 			
-			$blockContent=$this->getLayout()->createBlock('category.products');
+			$blockContent=$this->getLayout()->createBlock('content');
 			//$rootBlock->append($blockContent);			
 			//$this->getLayout()->getBlock('content')->append($blockContent);
 			
 			$this->renderLayout();
-			
+			*/
 			
 		}
 	}
