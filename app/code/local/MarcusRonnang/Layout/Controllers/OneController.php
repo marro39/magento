ed49710832e53e10187842c02a796cbb38aa4a1f
Marcus <marcusronnang@hotmail.com>
2014-05-09 20:01:48 +0200

    working with indexController.php, observer.php and config.xml in marcusronnang module to be able to load a page on menu click

---
+++ app/code/local/MarcusRonnang/Layout/Controllers/OneController.php
@@ -0,0 +1,8 @@
+<?php
+class MarcusRonnang_Layout_controllers_OneController extends Mage_Core_Controller_Front_Action {  
+      //indexAction is the default Action for any controller  
+      function indexAction() {  
+            echo "indexAction";  
+            
+      }  
+}  
