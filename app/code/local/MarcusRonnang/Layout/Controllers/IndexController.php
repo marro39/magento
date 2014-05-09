fbfe70a915acdd2b962c6c97d37f5d87ea6b0745
Marcus <marcusronnang@hotmail.com>
2014-05-09 18:59:58 +0200

    working with indexController.php, observer.php and config.xml in marcusronnang module to be able to load a page on menu click

--- app/code/local/MarcusRonnang/Layout/Controllers/IndexController.php
+++ app/code/local/MarcusRonnang/Layout/Controllers/IndexController.php
@@ -2,13 +2,14 @@
     class MarcusRonnang_Layout_IndexController extends Mage_Core_Controller_Front_Action{
     	
     	public function indexAction(){
+    		echo "NOW PAGE IS LOADED!!!!!!!!!!!!!!!!";
     		
     		var_dump(__METHOD__);
     		
-    		
+    		/*
     		$this->loadLayout();
     		$this->renderLayout();
-    		
+    		*/
     	} 
     }
 ?>
